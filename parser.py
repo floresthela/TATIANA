9a042cf4ce5cc799b459b41ec8d6f4401e138027
Flor Barbosa <esthelabarbosa@gmail.com>
2019-10-07 11:48:44 -0500

    Update parser.py

---
+++ parser.py
@@ -0,0 +1,64 @@
+import sys
+import ply.yacc as yacc
+from lexer import tokens
+
+# PROGRAM - flor
+def p_program(p):
+    '''
+
+    '''
+# STAR - flor
+def p_star(p):
+    '''
+
+    '''
+# LOOP - flor
+def p_loop(p):
+    '''
+    '''
+
+# STMT - flor
+def p_stmt(p):
+    '''
+    '''
+# VARS - flor
+def p_vars(p):
+    '''
+    '''
+
+# TYPE - flor
+def p_type(p):
+    '''
+    '''
+# PRINT - flor
+def p_print(p):
+    '''
+    '''
+# READ
+def p_read(p):
+    '''
+    '''
+# ASSIGNMENT
+# VAR_CTE - flor
+# CTE - flor
+# EXPRESSION - flor
+# RETURN 
+# COMPARE 
+# L_OP
+
+
+# LOGICAL
+# CONDITION
+# HEAD - flor 
+# BODY
+# FOR
+# WHILE
+# FUN_CALL
+# FUNCTION
+# GRAPH_FIGURE - flor
+# GRAPH_MOVEMENT - flor
+# GRAPH_REPEAT - flor
+# GRAPH_VIEW - flor
+# OPERATION
+# FACTOR
+# TERM
