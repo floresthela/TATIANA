6281a66b053485e1866e3891a0e001e1e31135c7
Flor Barbosa <esthelabarbosa@gmail.com>
2019-10-07 00:25:38 -0500

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -18,11 +18,54 @@ reserved = {
     'while' : 'WHILE'
 }
 
-#lista de tokens
+#tokens list
 tokens = [
     'id', 'dot', 'comma', 'equals',
     'openParen', 'closedParen', 'twoDots',
     'multiply', 'openBraces', 'closedBraces',
-    'plus', 'minus', 'division', 'cte_int', 'cte_float',
-    'cte_char', 'greater_than', 'less_than', 'not_equal'
-]
+    'plus', 'minus', 'division', 'cteint', 'ctefloat',
+    'ctechar', 'greater', 'less', 'notEqual'
+] + list(reserved.values())
+
+
+#tokens definition
+t_plus = r'\+'
+t_dot = r'\.'
+t_comma = r'\,'
+t_equals = r'\,'
+t_openParen = r'\('
+t_closedParen = r'\)'
+t_twoDots = r'\:'
+t_multiply = r'\*'
+t_openBraces = r'\{'
+t_closedBraces = r'\}'
+t_minus = r'\-'
+t_division = r'\/'
+t_greater = r'/>'
+t_less = r'/<'
+t_notEqual = r'/!='
+
+t_ignore = r' '
+
+def t_cteint(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_ctefloat(t):
+     r'\d+\.\d+'
+     t.value = float(t.value)
+     return t
+
+
+def t_ctechar(t):
+    r'[a-zA-Z][a-zA-Z_0-9]'
+    t.value = str(t.value)
+    return t
+
+
+def t_id(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'id')
+    return t
