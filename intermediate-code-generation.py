2b7eaec35c61487650ab2f4e1a1fea37e5dc5036
Flor Barbosa <esthelabarbosa@gmail.com>
2019-10-20 21:50:21 -0500

    Create intermediate-code-generation.py

---
+++ intermediate-code-generation.py
@@ -0,0 +1,26 @@
+# TATIANA
+# Flor Esthela Barbosa & Laura Santacruz
+
+# INTERMEDIATE-CODE GENERATION
+
+from semantic_cube import Operators, SemanticCube
+
+class Quadruple:
+    def __init__(self, left_op, right_op, operator, result):
+        '''
+        Inicializa cuádruplo
+        '''
+        self.left_op = left_op
+        self.right_op = right_op
+        self.operator = operator
+        self.result = result
+
+class Intermediate-CodeGeneration:
+    def __init__(self):
+        self.POper = []
+        self.PTypes = []
+        self.Quads = []
+        self.PilaO = []
+        self. PJumps = []
+
+    def genera_quad(self):
